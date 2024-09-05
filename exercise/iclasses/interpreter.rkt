19cbe85f8b633d1f15b5e1ec9b7041775fd0cc90
MarcosMateusOS <marcosmateusssantos09@gmail.com>
2024-09-05 09:33:36 -0300

    falta ajusta para passar o teste classes6 e o teste de while

--- exercise/iclasses/interpreter.rkt
+++ exercise/iclasses/interpreter.rkt
@@ -14,25 +14,25 @@
 
 ; value-of :: Exp -> ExpVal
 (define (value-of exp Δ)
-  (printf "exp => ~a\n" exp)
+  ; (printf "exp => ~a\n" exp)   para não exibir o log
   (match exp
     [(ast:int v) v]
     [(ast:bool v) v]
     [(ast:dif e1 e2)
      (let ([val1 (value-of e1 Δ)]
            [val2 (value-of e2 Δ)])
-       (printf "Subtraindo: ~a - ~a\n" val1 val2)
+       ; (printf "Subtraindo: ~a - ~a\n" val1 val2)  
        (- val1 val2))]
     [(ast:zero? e) (zero? (value-of e Δ))]
     [(ast:not e) (not (value-of e Δ))]
     [(ast:if e1 e2 e3) (if (value-of e1 Δ) (value-of e2 Δ) (value-of e3 Δ))]
     [(ast:var v)
      (let ([val (deref (apply-env Δ v))])
-       (printf "Variável encontrada: ~a, valor: ~a\n" v val)
+       ; (printf "Variável encontrada: ~a, valor: ~a\n" v val)  
        val)]
     [(ast:assign (ast:var x) e)
      (let ([value (value-of e Δ)])
-       (printf "Atribuindo ~a := ~a\n" x value)
+       ; (printf "Atribuindo ~a := ~a\n" x value)  
        (setref! (apply-env Δ x) value)  ; Atualiza o valor da variável no ambiente
        value)]  ; Retorna o valor atribuído
     [(ast:let (ast:var x) e1 e2)
@@ -57,32 +57,32 @@
 
 ; result-of :: Stmt -> Env -> State -> State
 (define (result-of stmt Δ)
-  (printf "Processando statement: ~a\n" stmt)
+  ; (printf "Processando statement: ~a\n" stmt)  
   (match stmt
     [(ast:assign (ast:var x) e)
      (begin
-       (printf "Atribuição: ~a := ~a\n" x (value-of e Δ))
+       ; (printf "Atribuição: ~a := ~a\n" x (value-of e Δ
+              ; (printf "Atribuição: ~a := ~a\n" x (value-of e Δ))  
        (setref! (apply-env Δ x) (value-of e Δ))
        42)]
-    [(ast:print e)
-     (begin
-       (printf "Imprimindo expressão: ~a\n" e)
-       (display (value-of e Δ))
-       (newline)
-       (value-of e Δ))]
+    [(ast:print e) 
+     (display (value-of e Δ)) 
+     (newline)]
     [(ast:return e)
      (begin
-       (printf "Retornando: ~a\n" e)
+       ; (printf "Retornando: ~a\n" e)  
        (value-of e Δ))]
     [(ast:block stmts)
      (for ([l stmts])
        (begin
-         (printf "Processando statement do bloco: ~a\n" l)  ; Depuração: imprime o statement `l`
-         (result-of l Δ)  ; Continua processando o bloco sem interromper
-         (printf "Processado statement do bloco: ~a\n" l)  ; Depuração: imprime o statement `l`
+         ; (printf "Processando statement do bloco: ~a\n" l)  
+         (result-of l Δ)
+         ; (printf "Processado statement do bloco: ~a\n" l)  
          #f))]  ; Garante que o bloco retorne #f após completar
-    [(ast:if-stmt e s1 s2) (display "if statement unimplemented")]
-    [(ast:while e s) (display "while unimplemented")]
+    [(ast:if-stmt e s1 s2) 
+     (display "if statement unimplemented")]
+    [(ast:while e s) 
+     (display "while statement unimplemented")]
     [(ast:local-decl (ast:var x) s)
      (result-of s (extend-env x (newref 'empty) Δ))]
     [(ast:send e (ast:var method-dcl) args)
@@ -100,19 +100,16 @@
 (define (apply-value-of exps Δ)
   (map (lambda (exp) (value-of exp Δ)) exps))
 
+
 (define (search_method class_name method_name)
-  (let ([class (search_class class_name)])
-    (if class
-        (let ([methods (class-methods class)])
-          (printf "Classe: ~a, Métodos: ~a\n" class_name (map car methods))
-          (for/or ([method methods])
-            (printf "Verificando método: ~a\n" (car method))
-            (if (equal? method_name (car method))
-                (car (cdr method))
-                (raise-user-error "Método não encontrado: " method_name))))
-        (raise-user-error "Classe não encontrada: " class_name))))
+    (let ([methods (class-methods (search_class class_name))])
+      (let ([found-method (assoc method_name methods)])
+        (if found-method
+            (cadr found-method)
+            (raise-user-error "error => método não implementado" method_name)))))
 
 (define (apply_method method self args)
+  ; (printf "Aplicando método: ~a com argumentos: ~a\n" (method-body method) args)  
   (let* ([args-refs (map newref args)]
          [class_env (extend-env "self" self (extend-env "super" (method-super_name method) empty-env))]
          [method_env (bind-vars (method-fields method) (object-fields self) class_env)]
@@ -121,7 +118,8 @@
 
 (define (new_object class_name value_args)
   (let* ([name_fields (class-fields (search_class class_name))]
-         [fields (map newref value_args)])
+         [fields (map (lambda (f_name) (newref null)) name_fields)])
+    ; (printf "Criando objeto da classe: ~a com campos: ~a\n" class_name fields)  
     (object class_name fields)))
 
 (define (search_class_in_program class_name)
@@ -140,6 +138,7 @@
 
 (define (bind-vars vars values env)
   (for ([var vars] [val values])
+      ; (printf "bind vars: ~a\n" var)  
     (set! env (extend-env var val env)))
   env)
 
@@ -192,10 +191,11 @@
        (for ([decl decls])
          (let* ([name (ast:var-name (ast:decl-name decl))]
                 [name_super (or (ast:var-name (ast:decl-super decl)) "object")]
-                (_ (printf "Superclasse: ~a\n" name_super))
+                ; (printf "Superclasse: ~a\n" name_super)  
                 [fields (append_fields (class-fields (search_class name_super))
                                        (map ast:var-name (ast:decl-fields decl)))]
                 [methods (define_methods (ast:decl-methods decl) name_super fields)]
                 [cls (class name_super fields methods)])
            (add_class name cls)))
+       ; (printf "Processamento completo do programa!\n")  
        (result-of stmt init-env))]))
